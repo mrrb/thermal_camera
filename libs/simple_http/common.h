25fe7f78301188fc96156560f6f588492157306c
Mario Rubio <mario@mrrb.eu>
2021-08-19 18:24:07 +0200

    Add strdup

---
+++ libs/simple_http/common.h
@@ -0,0 +1,22 @@
+/**
+ * \file common.h
+ * \author Mario Rubio (mario@mrrb.eu)
+ * \brief 
+ * \version 0.1
+ * \date 2021-02-18
+ */
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+char* common_strdup(const char* str);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* COMMON_H */
